c9207cf3acf20c8b746955470fcb03b423c05f47
wada <wadayugo@keio.jp>
2023-06-24 21:34:43 +0900

    basic types(week1)

---
+++ introductory-haskell-course/week1/basic_types.hs
@@ -0,0 +1,27 @@
+-- Machine-sized integers
+i :: Int
+i = -78
+
+-- Arbitrary-precision integers
+n :: Integer
+n = 1234567890987654321987340982334987349872349874534
+
+-- Double-precision floating point
+d1, d2 :: Double
+d1 = 4.5387
+d2 = 6.2831e-4
+
+-- Booleans
+b1, b2 :: Bool
+b1 = True
+b2 = False
+
+-- Unicode characters
+c1, c2, c3 :: Char
+c1 = 'x'
+c2 = 'Ø'
+c3 = 'ダ'
+
+-- Strings are lists of characters with special syntax
+s :: String
+s = "Hello, Haskell!!"
